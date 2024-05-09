e6ed9c348719efe6190e098d720414f80704aab6
jasonfangmagic <jasonfangmagic@gmail.com>
2024-05-09 11:44:02 -0400

    initial commit

--- Enum.playground/Contents.swift
+++ Enum.playground/Contents.swift
@@ -1,9 +1,10 @@
+import UIKit
 
-enum SocialPlatform {
-    case twitter
-    case facebook
-    case instagram
-    case linkedIn
+enum SocialPlatform: String, CaseIterable {
+    case twitter = "This is my favorite"
+    case facebook = "ok"
+    case instagram = "don't use"
+    case linkedIn = "need to post more"
 }
 
 func shareImage(on platform: SocialPlatform){
@@ -19,4 +20,33 @@ func shareImage(on platform: SocialPlatform){
     }
 }
 
-shareImage(on: .twitter)
+//shareImage(on: .twitter)
+
+func getSeanOpinion(on platform: SocialPlatform){
+    let opinion = platform.rawValue
+    print(opinion)
+}
+
+//getSeanOpinion(on: .twitter)
+
+print(SocialPlatform.allCases.count)
+
+for platform in SocialPlatform.allCases {
+    print(platform.rawValue)
+}
+
+enum SocialMediaPlatform {
+    case tweitter(followers: Int)
+    case youtube(subscribers: Int)
+}
+
+
+func getSponsorship(for platform: SocialMediaPlatform){
+    switch platform {
+    case .tweitter(let followers) where followers > 10000:
+        print("eligible")
+    case .youtube(let subscribers) where subscribers > 25000:
+        print("eligible")
+}
+
+getSponsorship(for: .tweitter(followers: 500))
